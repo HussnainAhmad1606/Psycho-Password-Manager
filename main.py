c4d94d356d6a27b2bcfee4aeb693d1528c7a68c0
Hussnain Ahmad <75199596+HussnainAhmad1606@users.noreply.github.com>
2022-09-05 23:13:47 +0500

    Encryption & Decryption
    Now we can store encrypted data to separate file and also can decrypt it. Total Number of Passwords are also showing on home.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from ast import main
 from tkinter import *
 from tkinter.messagebox import showinfo
-
+from cryptography.fernet import Fernet
+import os
 root = Tk()
 root.title("Psycho Password Manager")
 root.geometry("500x300")
@@ -9,15 +9,72 @@ root.geometry("500x300")
 websiteVar = StringVar()
 passwordVar = StringVar()
 emailVar = StringVar()
+totalPasswords = StringVar()
+
+if os.path.isfile("encrypt.txt"):
+    with open("encrypt.txt", 'r') as file:
+        data = file.readlines()
+
+    totalPasswords.set(f"Total Passwords: {len(data)}")
+else:
+    totalPasswords.set("Total Passwords: 0")
+
+def generate_key():
+    with open("mode.txt", "r") as file:
+        data = file.read()
+        print(data)
+    if data == "0":
+        key = Fernet.generate_key()
+        with open("secret.key", "wb") as key_file:
+            key_file.write(key)
+
+
+    with open("mode.txt", "w") as file:
+            file.write("1")
+    
+    root.config(menu=mainMenu)
+
+
+def load_key():
+    return open("secret.key", "rb").read()
+
+def encrypt_message(message):
+    key = load_key()
+    encoded_message = message.encode()
+    f = Fernet(key)
+    encrypted_message = f.encrypt(encoded_message)
+    return encrypted_message
+
+
+def exportAllPasswords():
+    key = load_key()
+    f = Fernet(key)
+    with open("encrypt.txt", "r") as file:
+        enc = file.readlines()
+    for index, data in enumerate(enc):
+        ency = bytes(data, 'utf-8')
+        decrypted_message = f.decrypt(ency)
+
+        print(decrypted_message.decode())
+        with open("decrypt.txt", "a") as file:
+            file.write(decrypted_message.decode())
+            file.write("\n")
 
 def savePassword():
-    pass
+    email = emailVar.get()
+    password = passwordVar.get()
+    website = websiteVar.get()
+    string = f'{website} {email} {password}'
+    message = encrypt_message(string)
+    print(message)
+    with open("encrypt.txt", "a") as file:
+        file.write(message.decode("utf-8"))
+        file.write("\n")
 
 def showAllPasswords():
     pass
 
-def exportAllPasswords():
-    pass
+
 
 def aboutSoftware():
     showinfo("About", "Software: Psycho Password Manager\nVersion: 1.0\nProgrammer: Psycho Coder")
@@ -39,7 +96,7 @@ emailEntry.pack()
 passwordLabel = Label(root, text="Password:")
 passwordLabel.pack()
 
-passwordEntry = Entry(root, textvariable=passwordVar, show=".")
+passwordEntry = Entry(root, textvariable=passwordVar, show="●")
 passwordEntry.pack()
 
 
@@ -66,6 +123,12 @@ optionsMenu = Menu(mainMenu, tearoff=0)
 optionsMenu.add_command(label="Show All Passwords", command=showAllPasswords)
 optionsMenu.add_command(label="Export All Passwords", command=exportAllPasswords)
 optionsMenu.add_separator()
+
+with open("mode.txt", "r") as file:
+    data = file.read()
+    if data == "0":
+        optionsMenu.add_command(label="Generate Key", command=generate_key)
+        optionsMenu.add_separator()
 optionsMenu.add_command(label="Exit", command=root.destroy)
 
 mainMenu.add_cascade(menu=optionsMenu, label="Options")
@@ -76,4 +139,10 @@ aboutMenu.add_command(label="About Software", command=aboutSoftware)
 mainMenu.add_cascade(menu=aboutMenu, label="About")
 
 root.config(menu=mainMenu)
+
+
+statusBar = Frame(root)
+statusBar.pack(side=BOTTOM)
+lines = Label(statusBar, textvariable=totalPasswords)
+lines.pack()
 root.mainloop()
